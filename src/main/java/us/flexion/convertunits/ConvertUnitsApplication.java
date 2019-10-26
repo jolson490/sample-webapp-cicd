7c58e65efb1842e6ab4f27991abfd293a5d4ff67
Joshua Olson <joshua.olson.490@gmail.com>
2019-10-26 16:44:26 -0500

    Add logging - via log4j2

--- src/main/java/us/flexion/convertunits/ConvertUnitsApplication.java
+++ src/main/java/us/flexion/convertunits/ConvertUnitsApplication.java
@@ -1,13 +1,23 @@
 package us.flexion.convertunits;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConvertUnitsApplication {
+  private static final Logger logger = LogManager.getLogger(ConvertUnitsApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(ConvertUnitsApplication.class, args);
   }
 
+  // Get a warm fuzzy relatively early on during initialization/startup of the application.
+  @PostConstruct
+  public void logSomething() {
+    logger.debug("in logSomething()");
+  }
 }
--- src/test/java/us/flexion/convertunits/ConvertUnitsApplicationTests.java
+++ src/test/java/us/flexion/convertunits/ConvertUnitsApplicationTests.java
@@ -1,13 +1,17 @@
 package us.flexion.convertunits;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConvertUnitsApplicationTests {
+  private static final Logger logger = LogManager.getLogger(ConvertUnitsApplicationTests.class);
 
   @Test
   void contextLoads() {
+    logger.debug("in contextLoads()");
   }
 
 }
