5b70de31fb97ce0f5f2e3a148c114db7b4a97d19
Joshua Olson <joshua.olson.490@gmail.com>
2019-10-26 01:40:34 -0500

    Initial commit - Spring Boot app that was generated with Spring Initializr.

---
+++ src/main/java/us/flexion/convertunits/ConvertUnitsApplication.java
@@ -0,0 +1,13 @@
+package us.flexion.convertunits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertUnitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertUnitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/flexion/convertunits/ConvertUnitsApplicationTests.java
@@ -0,0 +1,13 @@
+package us.flexion.convertunits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertUnitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
