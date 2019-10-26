2bdf132f68a2c7e193f50fe2e5ed0463203e330b
Joshua Olson <joshua.olson.490@gmail.com>
2019-10-26 12:08:54 -0500

    Apply formatting to code files.

--- src/main/java/us/flexion/convertunits/ConvertUnitsApplication.java
+++ src/main/java/us/flexion/convertunits/ConvertUnitsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConvertUnitsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConvertUnitsApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ConvertUnitsApplication.class, args);
+  }
 
 }
--- src/test/java/us/flexion/convertunits/ConvertUnitsApplicationTests.java
+++ src/test/java/us/flexion/convertunits/ConvertUnitsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConvertUnitsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
